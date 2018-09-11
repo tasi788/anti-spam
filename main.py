79008c962562c2fd125b31290ef5281fd196c4d2
踢低吸 <tasi788@gmail.com>
2018-09-11 17:58:51 +0800

    fix error stat

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def handle(msg):
 	content_type, chat_type, chat_id, msg_date, message_id = telepot.glance(
 		msg, long=True)
 	user_id = msg['from']['id']
-	botId = bot_apitoken.split(':')[0]
+	botId = int(bot_apitoken.split(':')[0])
 	fuckchannel = -1001229303409
 	username = msg['from']['first_name']
 	if 'last_name' in msg['from'].keys():
 		username += ' ' + msg['from']['last_name']
+
 	if content_type == 'new_chat_member':
 		gId = msg['chat']['id']
 		gName = msg['chat']['title']
@@ -56,25 +57,29 @@ def handle(msg):
 					bot.deleteMessage((chat_id, message_id))
 					print(tmp)
 					break
-				except telepot.exception.TelegramError as e:
-					tmp = '失敗惹 QQ\n' \
-						'group id: {gId}\n' \
-						'group name: {gName}\n' \
-						'name: {username}\n' \
-						'uid: {user_id}\n'.format(
-							gId=gId,
-							gName=gName,
-							username=username,
-							user_id=user_id
-						)
-					bot.sendMessage(fuckchannel, tmp)
-					print(tmp)
+				except Exception as e:
+					#Bad Request: message can't be deleted
+					permission = 'Bad Request: not enough rights to restrict/unrestrict chat member'
+					if e.description[0] == permission:
+						tmp = '我踢不走這個人[{username}](tg://user?id={user_id})\n' \
+						'因為你沒給我濫權 (´･_･`)'
+						bot.sendMessage(chat_id, tmp, parse_mode='markdown')
+						bot.leaveChat(chat_id)
+						tmp = '離開惹\n' \
+							'group id: `{gId}`\n' \
+							'group name: {gName}\n'
+						bot.sendMessage(fuckchannel, tmp, parse_mode='markdown')
+					print(e.description[0])
+
 	elif content_type == 'text':
 		say = msg['text'].lower()
 		if 'reply_to_message' in msg.keys() and user_id == int(owner) and say == '@admin':
 			reply_msgId = msg['reply_to_message']['message_id']
-			for x in [reply_msgId, message_id]:
-				bot.deleteMessage((chat_id, x))
+			try:
+				for x in [reply_msgId, message_id]:
+					bot.deleteMessage((chat_id, x))
+			except Exception as e:
+				print(e.description)
 
 
 
